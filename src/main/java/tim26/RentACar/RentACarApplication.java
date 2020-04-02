2e312cf729a7a450a7e5d0c1779481107d7b2b6b
tjncc <55355686+tjncc@users.noreply.github.com>
2020-04-02 20:24:12 +0200

    First commit

---
+++ src/main/java/tim26/RentACar/RentACarApplication.java
@@ -0,0 +1,13 @@
+package tim26.RentACar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentACarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentACarApplication.class, args);
+	}
+
+}
