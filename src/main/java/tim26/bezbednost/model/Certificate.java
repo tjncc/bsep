83eccd6f3c586221d26fe23647ddd00442882dc8
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-04-04 20:59:53 +0200

    updates

--- src/main/java/tim26/bezbednost/model/Certificate.java
+++ src/main/java/tim26/bezbednost/model/Certificate.java
@@ -1,4 +1,49 @@
 package tim26.bezbednost.model;
 
-public class Certificate {
+import org.apache.tomcat.jni.Local;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity
+public class Certificate  implements Serializable {
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="certificateRole", nullable = false)
+    private CertificateRole role;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="serialNumber", unique = true)
+    private String serialNumber;
+
+    public Certificate() {
+    }
+
+    public CertificateRole getRole() {
+        return role;
+    }
+
+    public void setRole(CertificateRole role) {
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
 }
---
+++ src/main/java/tim26/bezbednost/model/certificates/CertificateGenerator.java
@@ -0,0 +1,92 @@
+package tim26.bezbednost.model.certificates;
+
+import org.bouncycastle.asn1.ASN1ObjectIdentifier;
+import org.bouncycastle.asn1.x509.BasicConstraints;
+import org.bouncycastle.asn1.x509.Extension;
+import org.bouncycastle.cert.CertIOException;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+import org.springframework.stereotype.Component;
+
+import java.math.BigInteger;
+import java.security.*;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+@Component
+public class CertificateGenerator {
+
+    public CertificateGenerator() {
+        Security.addProvider(new BouncyCastleProvider());
+    }
+
+    public X509Certificate generateCertificate(SubjectData subjectData, IssuerData issuerData) {
+        try {
+            //Posto klasa za generisanje sertifiakta ne moze da primi direktno privatni kljuc pravi se builder za objekat
+            //Ovaj objekat sadrzi privatni kljuc izdavaoca sertifikata i koristiti se za potpisivanje sertifikata
+            //Parametar koji se prosledjuje je algoritam koji se koristi za potpisivanje sertifiakta
+            JcaContentSignerBuilder builder = new JcaContentSignerBuilder("SHA256WithRSAEncryption");
+            //Takodje se navodi koji provider se koristi, u ovom slucaju Bouncy Castle
+            builder = builder.setProvider("BC");
+
+            //Formira se objekat koji ce sadrzati privatni kljuc i koji ce se koristiti za potpisivanje sertifikata
+            ContentSigner contentSigner = builder.build(issuerData.getPrivateKey());
+
+            //Postavljaju se podaci za generisanje sertifiakta
+            X509v3CertificateBuilder certGen = new JcaX509v3CertificateBuilder(issuerData.getX500name(),
+                    new BigInteger(subjectData.getSerialNumber()),
+                    subjectData.getStartDate(),
+                    subjectData.getEndDate(),
+                    subjectData.getX500name(),
+                    subjectData.getPublicKey());
+            //Generise se sertifikat
+            X509CertificateHolder certHolder = certGen.build(contentSigner);
+
+            //Builder generise sertifikat kao objekat klase X509CertificateHolder
+            //Nakon toga je potrebno certHolder konvertovati u sertifikat, za sta se koristi certConverter
+            JcaX509CertificateConverter certConverter = new JcaX509CertificateConverter();
+            certConverter = certConverter.setProvider("BC");
+
+            //Konvertuje objekat u sertifikat
+            return certConverter.getCertificate(certHolder);
+        } catch (CertificateEncodingException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (OperatorCreationException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public KeyPair generateKeyPair(boolean isCertificateAuthority) {
+        try {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+            SecureRandom random = new SecureRandom();
+
+            //CA keysize is bigger for more secure private key
+            if(isCertificateAuthority) {
+                keyGen.initialize(4096, random);
+            } else {
+                keyGen.initialize(2048, random);
+            }
+            return keyGen.generateKeyPair();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
