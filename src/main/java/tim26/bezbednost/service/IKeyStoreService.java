985971a97b96d2a142fe26d33815f4a360a7dfc6
tjncc <55355686+tjncc@users.noreply.github.com>
2020-04-07 21:07:43 +0200

    Update IKeyStoreService.java

--- src/main/java/tim26/bezbednost/service/IKeyStoreService.java
+++ src/main/java/tim26/bezbednost/service/IKeyStoreService.java
@@ -1,9 +1,17 @@
 package tim26.bezbednost.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import tim26.bezbednost.model.enumeration.CertificateRole;
+
+import java.io.FileNotFoundException;
+import java.security.PrivateKey;
+import java.security.cert.X509Certificate;
+import java.util.List;
 
 public interface IKeyStoreService {
 
+    public void saveCertificate(X509Certificate certificate, String alias, PrivateKey privateKey, CertificateRole role);
+    public List<X509Certificate> findKeyStoreCertificates(CertificateRole role) throws FileNotFoundException;
 
 
 }
