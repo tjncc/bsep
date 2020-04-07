0e679b71db0634727c07b307bc91448de273ee23
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-04-07 22:46:02 +0200

    Update CertificateService.java

--- src/main/java/tim26/bezbednost/service/CertificateService.java
+++ src/main/java/tim26/bezbednost/service/CertificateService.java
@@ -1,6 +1,5 @@
 package tim26.bezbednost.service;
 
-import jdk.vm.ci.meta.Local;
 import org.bouncycastle.asn1.x500.X500NameBuilder;
 import org.bouncycastle.asn1.x500.style.BCStyle;
 import org.modelmapper.ModelMapper;
