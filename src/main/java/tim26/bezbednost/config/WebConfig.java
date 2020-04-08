f57ce3148bc5997362a4dab5163a15464ef2e4b4
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-04-08 14:39:11 +0200

    updates

--- src/main/java/tim26/bezbednost/config/WebConfig.java
+++ src/main/java/tim26/bezbednost/config/WebConfig.java
@@ -13,4 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("http://localhost:3000");
     }
+
+
 }
